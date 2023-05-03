34ebe4deb1c8c4834bb99d81e7eeacfad3014951
yunflow <dhsuicos@yahoo.com>
2023-05-03 16:27:14 -0300

    basic sort

---
+++ MyLib/src/sort_N2/Bubble.java
@@ -0,0 +1,23 @@
+package sort_N2;
+
+public class Bubble {
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ MyLib/src/sort_N2/Test.java
@@ -0,0 +1,7 @@
+package sort_N2;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
