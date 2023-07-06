0f056decbd3503a915ac2ba00108671c0e74489c
yunflow <dhsuicos@yahoo.com>
2023-07-06 00:56:50 -0300

    print

---
+++ MyLib/src/tree/Print.java
@@ -0,0 +1,11 @@
+package tree;
+
+public class Print {
+    public static void printLs(TreeNode head) {
+        System.out.println("Print for Tree:");
+        if (head == null) {
+            return;
+        }
+
+    }
+}
